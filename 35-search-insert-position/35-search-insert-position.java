41c335f6df5479ec3aa4f83ffd39c61f51d70f47
rishnt <94091947+rishnt@users.noreply.github.com>
2022-07-17 11:43:51 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (91.36%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -3,13 +3,7 @@ class Solution {
         
         int low=0,mid;
         int high=nums.length-1;
-        
-        if(target<nums[0]){
-            return 0;
-        }
-        if(target>nums[nums.length-1]){
-            return nums.length;
-        }
+     
         while(low<=high){
             mid=low+(high-low)/2;
             if(nums[mid]==target){
